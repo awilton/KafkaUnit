7c78fcaefa2ff7efee5766db592b5ab5142158f1
Art Wilton <arthur.wilton@vonage.com>
2015-06-12 13:09:04 -0400

    Added Utils package

---
+++ src/main/java/com/awilton/util/ReflectionUtils.java
@@ -0,0 +1,23 @@
+package com.awilton.util;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+
+public abstract class ReflectionUtils {
+  
+ 
+  public static void makeAccessible(Field field) {
+    if ((!Modifier.isPublic(field.getModifiers()) || 
+         !Modifier.isPublic(field.getDeclaringClass().getModifiers()) ||
+         Modifier.isFinal(field.getModifiers())) &&!field.isAccessible())
+    {
+      field.setAccessible(true);
+    }   
+  }
+  
+  public static void inject(Field f, Object o) throws Throwable {
+    makeAccessible(f);
+    f.set(o, f);
+  }
+  
+}
